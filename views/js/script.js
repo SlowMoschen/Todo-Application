666cb33a7dd8cd7dfb7b4d0bea7019826edaa614
Millner Philipp <philipp.millner@acp.at>
2023-09-05 15:33:50 +0200

    JS for Frontend updated

--- views/js/script.js
+++ views/js/script.js
@@ -4,11 +4,21 @@ addButton = document.querySelector('.add-button'),
 addForm = document.querySelector('#addForm'),
 closeModalBtn = document.querySelector('.close-btn');
 
+
+// Eventlisteners
 addButton.addEventListener("click", (e) => {
     addClass(addModal, 'show')
 })
 
 closeModalBtn.addEventListener("click", () => {
+    removeClass(addModal, 'show')
+})
+
+addForm.addEventListener("submit", (e) => {
+    const input = document.querySelector('#todo-input')
+    e.preventDefault()
+    addTodo(input.value)
+    input.value = ""
     removeClass(addModal, 'show')
 })
 
@@ -31,3 +41,111 @@ function removeClass(element, className) {
         element.classList.remove(className)
     }
 }
+
+// API Fetches
+const todoList = document.querySelector('#todo-list')
+const API = 'http://localhost:3001/tasks'
+let todoArray = [
+    {
+        todo: 'Tets Todo',
+        state: false,
+    },
+]
+
+async function getAllTodos() {
+    try {
+        const respone = await fetch(API)
+        const data = respone.json()
+        console.log(data);
+        todoArray = [...todoArray, data]
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function addTodo(todoValue) {
+    
+    const todo = {
+        todo: todoValue,
+        state: false,
+        id: todoArray.length + 1
+    }
+    
+    try {
+        const respone = await fetch(API, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(todo)
+        })
+        
+        createTodoElement(todo)
+        console.log(respone.json());
+        
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+// Logic to add Todos
+
+function createTodoElement (obj) {
+        const liEL = document.createElement('li')
+            liEL.id = 'todo';
+            liEL.innerHTML = `
+            <div class="checkbox-rect2">
+                <input type="checkbox" id="checkbox-rect2" name="check">
+            </div>
+            <div id="todo-text">
+                ${obj.todo}
+            </div>
+            <div id="delete-btn">
+                <span class="material-symbols-outlined" id="delete">close</span>
+            </div>
+            `
+            todoList.appendChild(liEL)
+    addEventListeners()
+    return;
+}
+
+function addEventListeners() {
+    const inputBoxes = document.querySelectorAll('#checkbox-rect2')
+    const deleteBtns = document.querySelectorAll('#delete-btn')
+
+    inputBoxes.forEach(box => {
+        box.addEventListener("click", (e) => {
+            const target =  e.currentTarget.parentElement.parentElement
+            markAsComplete(target)
+        })
+    })
+    
+    deleteBtns.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            const target =  e.currentTarget.parentElement
+            console.log(target);
+            deleteTodo(target)
+        })
+    })
+}
+
+function markAsComplete(todo) {
+    const inputBox = todo.querySelector('#checkbox-rect2')
+    const todoTextEL = todo.querySelector('#todo-text')
+    const todoValue = todoTextEL.textContent
+
+    if(inputBox.checked){
+        todoTextEL.innerHTML = `<s>${todoValue}</s>`
+    } else {
+        todoTextEL.innerHTML = todoValue
+    }
+}
+
+function deleteTodo(todo) {
+    todo.remove()
+}
+
+todoArray.forEach(el => {
+    createTodoElement(el)
+})
